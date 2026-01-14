ceb14acbc3b60cfd8b8edce96046c942a3343043
ZABDI SAMUEL MILLAN JAIMES <20253ds151@utez.edu.mx>
2026-01-13 22:15:54 -0600

    Tarea 01: Estructura inicial y hola mundo

---
+++ proyecto-01-hola-mundo/src/Hello.java
@@ -0,0 +1,9 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("¡Hola, Mundo!");
+        System.out.println("MILLAN JAIMES ZABDI SAMUEL");
+        System.out.println("2°E");
+        System.out.println("24 de Enero del 2026");
+        
+    }
+}
